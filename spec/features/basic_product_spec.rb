5cc048e2bf6bbdf00d9f34d751a75194c89937ef
Matthew Kennedy <m.kennedy@me.com>
2020-05-23 21:44:35 +0100

    Beef up testing

--- spec/features/basic_product_spec.rb
+++ spec/features/basic_product_spec.rb
@@ -1,30 +1,66 @@
 require 'spec_helper'
 
-describe 'Tests A Basic Product Added To The Feed', type: :feature, js: true do
+describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
   stub_authorization!
 
-  context 'When a basic product is set to be shown in the product feed' do
-    let(:product) do
-      FactoryBot.create(:base_product, feed_active: true, unique_identifier: '80250-95240', unique_identifier_type: 'mpn' )
-    end
+  let(:product) do
+    FactoryBot.create(:base_product, feed_active: true, unique_identifier: '80250-95240', unique_identifier_type: 'mpn' )
+  end
 
-    before do
+  context 'When a product is set to be shown in the product feed' do
+    it 'it adds the product item and id correctly' do
       product.tap(&:save)
-
       visit "/products.rss"
-    end
-
-    it 'it adds the basic product id correctly' do
+      
       xml = Capybara.string(page.body)
-
       expect(xml).to have_text('<g:id>1-1</g:id>')
-    end
-
-    it 'it add the basic product unique_identifier and unique_identifier_type correctly' do
-      xml = Capybara.string(page.body)
-
-      expect(xml).to have_text('<g:id>1-1</g:id>')
-      expect(xml).to have_text('<g:mpn>80250-95240</g:mpn>')
     end
   end
+
+  context 'Test Unique Idetifiers' do
+    it 'it has type GTIN with new code' do
+      product.update(unique_identifier: '8025082379237', unique_identifier_type: 'gtin')
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<g:id>1-1</g:id>')
+      expect(xml).to have_text('<g:gtin>8025082379237</g:gtin>')
+    end
+
+    it 'it has type MPN with new code' do
+      product.update(unique_identifier: 'MPN901222', unique_identifier_type: 'mpn')
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<g:id>1-1</g:id>')
+      expect(xml).to have_text('<g:mpn>MPN901222</g:mpn>')
+    end
+  end
+
+  context 'Test product availability renderes XML availability correctly' do
+    it 'When count on hand is 0 and backorderable is false, the availability is OUT OF STOCK' do
+      product.master.stock_items.update_all count_on_hand: 0, backorderable: false
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<g:availability>out of stock</g:availability>')
+    end
+
+    it 'When count on hand is 3 and backorderable is true, the availability is IN STOCK' do
+      product.master.stock_items.update_all count_on_hand: 3, backorderable: true
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<g:availability>in stock</g:availability>')
+    end
+
+    it 'When count on hand is 0 and backorderable is true, the availability is OUT OF STOCK' do
+      product.master.stock_items.update_all count_on_hand: 0, backorderable: true
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<g:availability>out of stock</g:availability>')
+    end
+  end
+
 end
