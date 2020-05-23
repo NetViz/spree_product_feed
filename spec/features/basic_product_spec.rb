34032250106640ef815ae1a4694a63bd162a59f5
Matthew Kennedy <m.kennedy@me.com>
2020-05-23 22:42:20 +0100

    Write more tests

--- spec/features/basic_product_spec.rb
+++ spec/features/basic_product_spec.rb
@@ -1,24 +1,30 @@
 require 'spec_helper'
 
-describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
+describe 'Test A Basic Product In The Feed', type: :feature, js: true do
   stub_authorization!
 
   let(:product) do
-    FactoryBot.create(:base_product, feed_active: true, unique_identifier: '80250-95240', unique_identifier_type: 'mpn' )
+    FactoryBot.create(:base_product,
+      name: 'Mens Socks',
+      meta_description: 'Pair of mens socks',
+      feed_active: true, unique_identifier: '80250-95240',
+      unique_identifier_type: 'mpn' )
   end
 
-  context 'When a product is set to be shown in the product feed' do
-    it 'it adds the product item and id correctly' do
+  context 'A product set to be shown in the product feed' do
+    it 'shows the xml item, id, name and description correctly' do
       product.tap(&:save)
       visit "/products.rss"
-      
+
       xml = Capybara.string(page.body)
       expect(xml).to have_text('<g:id>1-1</g:id>')
+      expect(xml).to have_text('<g:title>Mens Socks</g:title>')
+      expect(xml).to have_text('<g:description>Pair of mens socks</g:description>')
     end
   end
 
-  context 'Test Unique Idetifiers' do
-    it 'it has type GTIN with new code' do
+  context 'Check the unique identifier' do
+    it 'shows type GTIN with the correct barcode' do
       product.update(unique_identifier: '8025082379237', unique_identifier_type: 'gtin')
       visit "/products.rss"
 
@@ -27,7 +33,7 @@ describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
       expect(xml).to have_text('<g:gtin>8025082379237</g:gtin>')
     end
 
-    it 'it has type MPN with new code' do
+    it 'shows type MPN with the correct manufacturers part number' do
       product.update(unique_identifier: 'MPN901222', unique_identifier_type: 'mpn')
       visit "/products.rss"
 
@@ -37,8 +43,8 @@ describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
     end
   end
 
-  context 'Test product availability renderes XML availability correctly' do
-    it 'When count on hand is 0 and backorderable is false, the availability is OUT OF STOCK' do
+  context 'Make sure the product availability' do
+    it 'shows OUT OF STOCK when count on hand is 0 and backorderable is false' do
       product.master.stock_items.update_all count_on_hand: 0, backorderable: false
       visit "/products.rss"
 
@@ -46,7 +52,7 @@ describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
       expect(xml).to have_text('<g:availability>out of stock</g:availability>')
     end
 
-    it 'When count on hand is 3 and backorderable is true, the availability is IN STOCK' do
+    it 'shows IN STOCK when count on hand is 3 and backorderable is true' do
       product.master.stock_items.update_all count_on_hand: 3, backorderable: true
       visit "/products.rss"
 
@@ -54,7 +60,7 @@ describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
       expect(xml).to have_text('<g:availability>in stock</g:availability>')
     end
 
-    it 'When count on hand is 0 and backorderable is true, the availability is OUT OF STOCK' do
+    it 'shows OUT OF STOCK when count on hand is 0 and backorderable is true' do
       product.master.stock_items.update_all count_on_hand: 0, backorderable: true
       visit "/products.rss"
 
@@ -63,4 +69,32 @@ describe 'Tests A Basic Product In The Feed', type: :feature, js: true do
     end
   end
 
+  context 'When the product copare price is set it' do
+    it 'shows sale price if compare at price is higher than master price' do
+      product.master.prices.first.update(compare_at_amount: 69.99)
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<g:sale_price>19.99 USD</g:sale_price>')
+      expect(xml).to have_text('<g:price>69.99 USD</g:price>')
+    end
+
+    it 'does not show a sale price if compare at price is less than selling price' do
+      product.master.prices.first.update(compare_at_amount: 9.99)
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).not_to have_text('<g:sale_price>9.99 USD</g:sale_price>')
+      expect(xml).to have_text('<g:price>19.99 USD</g:price>')
+    end
+
+    it 'does not show a sale price if compare at price nil' do
+      product.master.prices.first.update(compare_at_amount: nil)
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).not_to have_text('<g:sale_price>9.99 USD</g:sale_price>')
+      expect(xml).to have_text('<g:price>19.99 USD</g:price>')
+    end
+  end
 end
--- spec/features/props_spec.rb
+++ spec/features/props_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 describe 'Tests Product Properties', type: :feature, js: true do
   stub_authorization!
 
-  context 'A product property with presentation set to product_feed' do
+  context 'A product property with presentation set to product_feed and value set' do
     let(:property) { create(:property) }
     let(:prop1) { create(:property, name: 'g:product_type', presentation: 'product_feed') }
     let(:product) { create(:product, properties: [prop1], price: '49.99', feed_active: true, unique_identifier: '80250-95240', unique_identifier_type: 'mpn' ) }
 
     before do
       product.tap(&:save)
-
+      product.product_properties.first.update(value: 'Mens Baggy Socks')
       visit "/products.rss"
     end
 
-    it "Shows in the feed" do
+    it "shows the correct xml tag name and value in feed" do
       xml = Capybara.string(page.body)
-      expect(xml).to have_text('<g:product_type/>')
+      expect(xml).to have_text('<g:product_type>Mens Baggy Socks</g:product_type>')
     end
   end
 end
