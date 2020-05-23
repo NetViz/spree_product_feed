f62102bb3a2ce777fe2decd427a2a560f6af54a2
Matthew Kennedy <m.kennedy@me.com>
2020-05-23 19:48:27 +0100

    Improve store details spec

--- spec/features/store_spec.rb
+++ spec/features/store_spec.rb
@@ -1,24 +1,35 @@
 require 'spec_helper'
 
-describe 'Test Store Details Are Added To The Feed', type: :feature, js: true do
+describe 'Set store values in xml feed', type: :feature, js: true do
   stub_authorization!
 
-  context 'Set the store details' do
-    let(:store) { Spree::Store.default }
-    let(:store_name) do
-      ((first_store = Spree::Store.first) && first_store.name).to_s
-    end
+  let(:store) { Spree::Store.default }
 
-    before do
+  context 'If no preferances are set' do
+
+    it 'it renders the default store details correctly' do
       visit "/products.rss"
-    end
 
-    it 'it renders the store details correctly' do
       xml = Capybara.string(page.body)
 
       expect(xml).to have_text('<title>Spree Test Store</title>')
       expect(xml).to have_text('<link>www.example.com</link>')
       expect(xml).to have_text('<language>en-us</language>')
+    end
+  end
+
+  context 'When custom properties are set' do
+    before do
+      store.update(url: 'www.therubberfactory.com', name: 'The Rubber Factory', default_locale: 'en')
+    end
+
+    it 'the feed displays custom title, link and language' do
+      visit "/products.rss"
+
+      xml = Capybara.string(page.body)
+      expect(xml).to have_text('<title>The Rubber Factory</title>')
+      expect(xml).to have_text('<link>www.therubberfactory.com</link>')
+      expect(xml).to have_text('<language>en</language>')
     end
   end
 end
